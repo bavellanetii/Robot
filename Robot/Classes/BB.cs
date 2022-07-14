808d096ccf62449ea1af661ecc5fb2069e323991
bavellanetii <78850672+bavellanetii@users.noreply.github.com>
2022-07-14 11:40:07 +0100

    Created class for BB warehouse and added SQLite package

---
+++ Robot/Classes/BB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Robot.Classes
+{
+    class BB
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Product { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Bin { get; set; }
+
+        public string LotNumber { get; set; }
+    }
+}
